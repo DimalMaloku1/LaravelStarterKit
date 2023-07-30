1e1ec24ed0d6cebf6b9342e4b9c2ed77f5f4d3dd
DimalMaloku1 <dm51522@ubt-uni.net>
2023-07-30 21:49:04 +0200

    Added edit and delete routes for managing products

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,8 @@ Route::group(['prefix'=> 'admin'],function(){
         Route::post('/categories',[CategoryController::class,'store'])->name('categories.store');
         Route::get('/categories/{category}/edit',[CategoryController::class,'edit'])->name('categories.edit');
         Route::put('/categories/{category}',[CategoryController::class,'update'])->name('categories.update');
+        Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.delete');
+
 
         //temp-images.create
         Route::post('/upload-temp-image',[TempImagesController::class,'create'])->name('temp-images.create');
